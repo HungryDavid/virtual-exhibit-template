29f61e3431e502c6e00f3f4964bff92d9f9c7211
zabalance21 <ronin_zerna@dlsu.edu.ph>
2026-07-05 14:51:29 +0800

    added images to Structures

--- src/components/Structures.jsx
+++ src/components/Structures.jsx
@@ -1,10 +1,17 @@
+import arpa83 from '../assets/ARPA83.jpeg'; 
+import first from '../assets/First.jpeg';
+import expansion from '../assets/Expansion.jpg';
+import tcp from '../assets/TCP.png';
+import dns from '../assets/DNS.jpeg'
+
+
 export default function Foundation() {
   const milestones = [
-    { year: '1973', title: 'International Expansion of ARPANET' },
-    { year: '1974', title: 'First ISP is born & Proposal of TCP' },
-    { year: '1983', title: 'TCP/IP is standardized' },
-    { year: '1984', title: 'DNS introduced' },
-    { year: '1985', title: 'First registered domain name' },
+    { year: '1973', title: 'International Expansion of ARPANET' , name:'expansion', img: expansion},
+    { year: '1974', title: 'First ISP is born & Proposal of TCP', name:'tcp', img: tcp},
+    { year: '1983', title: 'TCP/IP is standardized', name:'arpa83', img: arpa83},
+    { year: '1984', title: 'DNS introduced', name: 'dns', img: dns},
+    { year: '1985', title: 'First registered domain name', name:'first', img: first},
   ];
 
   return (
@@ -22,7 +29,7 @@ export default function Foundation() {
             <div key={i} className="flex flex-col items-center w-64">
               <h3 className="font-bold text-center mb-2 w-70 text-white">{m.title}</h3>
               <div className="w-75 h-70 bg-gray-200 rounded-lg flex items-center justify-center border border-gray-300">
-                Image {i + 1}
+                <img src={m.img.src} alt={m.title} className="w-full h-full object-cover rounded-lg" />
               </div>
               <p className="bg-white mt-4 text-lg font-mono text-black rounded-xl px-10">
                 {m.year}
@@ -37,7 +44,7 @@ export default function Foundation() {
             <div key={i + 3} className="flex flex-col items-center w-64">
               <h3 className="font-bold text-center mb-2 w-70 text-white">{m.title}</h3>
               <div className="w-75 h-70 bg-gray-200 rounded-lg flex items-center justify-center border border-gray-300">
-                Image {i + 4}
+                <img src={m.img.src} alt={m.title} className="w-full h-full object-cover rounded-lg"/>
               </div>
               <p className="bg-white mt-4 text-lg font-mono text-black rounded-xl px-10">
                 {m.year}
